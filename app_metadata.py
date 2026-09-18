59992698585638f0fda80bb35edeeeee54094f05
mizontheprogrammer <misharecente28@gmail.com>
2026-09-19 01:48:11 +0800

    Bump version to 1.0.2

--- app_metadata.py
+++ app_metadata.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 APP_NAME = "Codex Indicator"
-APP_VERSION = "1.0.1"
-APP_VERSION_TUPLE = (1, 0, 1, 0)
+APP_VERSION = "1.0.2"
+APP_VERSION_TUPLE = (1, 0, 2, 0)
 
 __all__ = ["APP_NAME", "APP_VERSION", "APP_VERSION_TUPLE"]
